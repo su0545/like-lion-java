6c046a6c8f088884f907ee99b12aabe50749422a
전수진 <sjning94@naver.com>
2022-10-05 10:49:26 +0900

    first commit 1. a와 b의 값을 바꾸는 swap 로직

---
+++ src/week03/Calculator.java
@@ -0,0 +1,19 @@
+package week03;
+
+public class Calculator {
+    public void plus(int first, int second) {
+        System.out.println(first + second);
+    }
+
+    public void minus(int first, int second) {
+        System.out.println(first - second);
+    }
+
+    public void multiple(int first, int second) {
+        System.out.println(first * second);
+    }
+
+    public void divide(int first, int second) {
+        System.out.println(first / second);
+    }
+}
---
+++ src/week03/CalculatorMain.java
@@ -0,0 +1,13 @@
+package week03;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator =new Calculator();
+
+        calculator.plus(10, 20);
+        calculator.minus(10, 20);
+        calculator.multiple(10, 20);
+        calculator.divide(10, 20);
+    }
+}
+
