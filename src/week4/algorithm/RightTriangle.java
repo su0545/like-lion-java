71014b6a1c830eed44c98d6bfc336f18637ac6c8
전수진 <sjning94@naver.com>
2022-10-17 09:43:02 +0900

    10.17 별찍기

--- src/week4/algorithm/RightTriangle.java
+++ src/week4/algorithm/RightTriangle.java
@@ -1,11 +1,20 @@
 package week4.algorithm;
 
 public class RightTriangle {
-    public static void main(String[] args) {
 
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+    public void printtStar(int n){
+
+        for (int i = 0; i < n; i++){
+            //for문을 한개 더 쓰고 println말고 print, printf등을 써서 한줄로 출력하기
+        for (int j = 0; j <= ; j++) {
+            System.out.println("*");
+        }
+        System.out.println("\n");
+            }
+        }
+
+    public static void main(String[] args) {
+        RightTriangle rt = new RightTriangle();
+        rt.printtStar(10);
     }
 }
