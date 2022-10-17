d594b42abb085f5eb8deb5abfd88cd092034b5ce
전수진 <sjning94@naver.com>
2022-10-17 09:10:47 +0900

    10.17 별찍기

---
+++ src/week4/algorithm/RightTriangle.java
@@ -0,0 +1,11 @@
+package week4.algorithm;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
